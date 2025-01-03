aed9c6529835de07202456f92444002042f2f71b
ER1CK122 <erckgames53@gmail.com>
2025-01-03 16:54:29 -0300

    Criado o componente de posts. Alterado de export default para export named. IssueId#03

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-export default function App() {
+import { Post } from "./Post";
+
+export function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Post
+        author="Erick Nunes"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, quos molestiae? Eius aliquam ipsum voluptates, distinctio inventore doloremque ut iste, perferendis repudiandae neque non, atque reprehenderit consequatur labore consectetur praesentium."
+      />
     </>
+    
   )
 }
