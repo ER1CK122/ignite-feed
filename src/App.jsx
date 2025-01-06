aa2f159be461045cc3c29d377eac3bf0dca13578
ER1CK122 <erckgames53@gmail.com>
2025-01-05 23:27:14 -0300

    Adicionando estilos globais para o projeto. IssueId#04

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
 
 export function App() {
   return (
     <>
+      <Header />
+      
       <Post
         author="Erick Nunes"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, quos molestiae? Eius aliquam ipsum voluptates, distinctio inventore doloremque ut iste, perferendis repudiandae neque non, atque reprehenderit consequatur labore consectetur praesentium."
